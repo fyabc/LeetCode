7425312b607d9295529765d77e6cac4cd1a9d03e
fyabc <fyabc@mail.ustc.edu.cn>
2020-01-01 05:29:18 +0800

    Solve problem 61 ~ 67.

--- leetcode/problem61to70.cpp
+++ leetcode/problem61to70.cpp
@@ -5,25 +5,193 @@
 #include "support/ForwardList.h"
 #include "support/IO.h"
 
+#include <algorithm>
+#include <bitset>
+#include <regex>
+
 using namespace std;
 
 class Solution61 {
 public:
     static ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
         auto p = head;
         int N = 0;
-        ListNode* newHead = nullptr, *last = nullptr;
+        ListNode* newTail = nullptr, *tail = nullptr;
 
-        leetcode::printlnV();
-        for (; p != nullptr; p = p->next, ++N) {
-            last = p;
-            leetcode::printlnV("At:", N, "val:", p->val);
-            if (N == k)
-                newHead = p;
+        for (; p != nullptr; p = p->next, ++N)
+            tail = p;
+
+        k %= N;
+        if (k == 0)
+            return head;
+
+        newTail = head;
+        for (int i = 0; i < N - k - 1; ++i)
+            newTail = newTail->next;
+
+        auto newHead = newTail->next;
+
+        tail->next = head;
+        newTail->next = nullptr;
+
+        return newHead;
+    }
+};
+
+
+class Solution62 {
+public:
+    static int uniquePaths(int m, int n) {
+        return combination(m + n - 2, max(m - 1, n - 1));
+    }
+
+private:
+    static int combination(int m, int n) {
+        long long r = 1;
+        for (long long i = n + 1; i <= m; ++i) {
+            r *= i;
+            r /= (i - n);
+        }
+        return static_cast<int>(r);
+    }
+};
+
+class Solution63 {
+public:
+    static int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid.empty())
+            return 0;
+        if (obstacleGrid[0].empty())
+            return 0;
+
+        auto M = obstacleGrid.size(), N = obstacleGrid[0].size();
+
+        vector<long long> rowValues(N, 0);
+
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (obstacleGrid[i][j] == 1)
+                    rowValues[j] = 0;
+                else {
+                    if (i == 0) {
+                        if (j == 0)
+                            rowValues[j] = 1;
+                        else
+                            rowValues[j] = rowValues[j - 1];
+                    } else {
+                        if (j == 0) {
+                            // Do nothing
+                        } else
+                            rowValues[j] += rowValues[j - 1];
+                    }
+                }
+            }
         }
 
-        leetcode::printlnV("N:", N);
+        return static_cast<int>(rowValues[N - 1]);
+    }
+};
 
-        return nullptr;
+class Solution64 {
+public:
+    static int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty())
+            return 0;
+        if (grid[0].empty())
+            return 0;
+
+        auto M = grid.size(), N = grid[0].size();
+
+        vector<int> rowValues(N, 0);
+
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (i == 0) {
+                    if (j == 0)
+                        rowValues[j] = grid[i][j];
+                    else
+                        rowValues[j] = rowValues[j - 1] + grid[i][j];
+                } else {
+                    if (j == 0)
+                        rowValues[j] += grid[i][j];
+                    else
+                        rowValues[j] = min(rowValues[j], rowValues[j - 1]) + grid[i][j];
+                }
+            }
+        }
+
+        return rowValues[N - 1];
+    }
+};
+
+class Solution65 {
+    static const regex PatternFloatNumber;
+public:
+    inline static bool isNumber(const string& s) {
+        return regex_match(s, PatternFloatNumber);
+    }
+};
+
+const regex Solution65::PatternFloatNumber {R"(^\s*[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?\s*$)"};
+
+class Solution66 {
+public:
+    static vector<int> plusOne(vector<int>& digits) {
+        int N = static_cast<int>(digits.size());
+        int v = 1;
+        for (int i = N - 1; i >= 0; --i) {
+            digits[i] += v;
+            if (digits[i] >= 10) {
+                digits[i] = 0;
+                v = 1;
+            } else {
+                v = 0;
+                break;
+            }
+        }
+        if (v != 0) {
+            digits.insert(digits.begin(), v);
+        }
+        return digits;
+    }
+};
+
+
+class Solution67 {
+    static constexpr char Int2Char[] = {'0', '1'};
+public:
+    static string addBinary(const string& a, const string& b) {
+        auto M = a.size(), N = b.size();
+        string result(max(M, N) + 1, '0');
+        int v = 0;
+        for (int i = 0; i < result.size(); ++i) {
+            int val = ch2i(i >= M ? '0' : a[M - 1 - i]) + ch2i(i >= N ? '0' : b[N - 1 - i]) + v;
+            if (val >= 2) {
+                val -= 2;
+                v = 1;
+            } else
+                v = 0;
+            result[i] = Int2Char[val];
+        }
+        auto it = result.rbegin();
+        while (*it == '0') ++it;
+        if (it == result.rend())
+            return "0";
+        return string(it, result.rend());
+    }
+
+private:
+    static int ch2i(char ch) {
+        return ch - '0';
+    }
+};
+
+class Solution68 {
+public:
+    static vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        return {};
     }
 };
--- test/leetcode/testProblem61to80.cpp
+++ test/leetcode/testProblem61to80.cpp
@@ -18,4 +18,109 @@ TEST(Problem61Test, Basic) {
     auto v2 = ListNode::fromInitList({0, 1, 2});
     auto r2 = ListNode::fromInitList({2, 0, 1});
     EXPECT_EQ(s.rotateRight(v2, 4)->toVector(), r2->toVector());
+
+    auto v3 = ListNode::fromInitList({1, 2, 3, 4, 5});
+    auto r3 = ListNode::fromInitList({1, 2, 3, 4, 5});
+    EXPECT_EQ(s.rotateRight(v3, 0)->toVector(), r3->toVector());
+    EXPECT_EQ(s.rotateRight(v3, 5)->toVector(), r3->toVector());
+
+    auto v4 = ListNode::fromInitList({1, 2});
+    auto r4 = ListNode::fromInitList({2, 1});
+    EXPECT_EQ(s.rotateRight(v4, 1)->toVector(), r4->toVector());
+}
+
+TEST(Problem62Test, Basic) {
+    Solution62 s;
+
+    EXPECT_EQ(s.uniquePaths(3, 2), 3);
+    EXPECT_EQ(s.uniquePaths(7, 3), 28);
+    EXPECT_EQ(s.uniquePaths(10, 10), 48620);
+    EXPECT_EQ(s.uniquePaths(51, 9), 1916797311);
+}
+
+TEST(Problem63Test, Basic) {
+    Solution63 s;
+
+    vector<vector<int>> v1 = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+    EXPECT_EQ(s.uniquePathsWithObstacles(v1), 2);
+
+    vector<vector<int>> v2 = {{0, 0}};
+    EXPECT_EQ(s.uniquePathsWithObstacles(v2), 1);
+}
+
+TEST(Problem64Test, Basic) {
+    Solution64 s;
+
+    vector<vector<int>> v1 = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    EXPECT_EQ(s.minPathSum(v1), 7);
+}
+
+TEST(Problem65Test, Basic) {
+    Solution65 s;
+
+    EXPECT_EQ(s.isNumber("0"), true);
+    EXPECT_EQ(s.isNumber("0.1"), true);
+    EXPECT_EQ(s.isNumber("abc"), false);
+    EXPECT_EQ(s.isNumber("1 a"), false);
+    EXPECT_EQ(s.isNumber("2e10"), true);
+    EXPECT_EQ(s.isNumber(" -90e3   "), true);
+    EXPECT_EQ(s.isNumber("e3"), false);
+    EXPECT_EQ(s.isNumber("6e-1"), true);
+    EXPECT_EQ(s.isNumber(" 99e2.5"), false);
+    EXPECT_EQ(s.isNumber("53.5e93"), true);
+    EXPECT_EQ(s.isNumber(" --6"), false);
+    EXPECT_EQ(s.isNumber("-+3"), false);
+    EXPECT_EQ(s.isNumber("95a54e53"), false);
+    EXPECT_EQ(s.isNumber("3."), true);
+}
+
+TEST(Problem66Test, Basic) {
+    Solution66 s;
+
+    vector<int> v1 = {1, 2, 3};
+    vector<int> r1 = {1, 2, 4};
+    EXPECT_EQ(s.plusOne(v1), r1);
+
+    vector<int> v2 = {4, 3, 2, 1};
+    vector<int> r2 = {4, 3, 2, 2};
+    EXPECT_EQ(s.plusOne(v2), r2);
+
+    vector<int> v3 = {1, 9, 9, 9};
+    vector<int> r3 = {2, 0, 0, 0};
+    EXPECT_EQ(s.plusOne(v3), r3);
+
+    vector<int> v4 = {0};
+    vector<int> r4 = {1};
+    EXPECT_EQ(s.plusOne(v4), r4);
+
+    vector<int> v5 = {9, 9};
+    vector<int> r5 = {1, 0, 0};
+    EXPECT_EQ(s.plusOne(v5), r5);
+}
+
+TEST(Problem67Test, Basic) {
+    Solution67 s;
+
+    EXPECT_EQ(s.addBinary("11", "1"), "100");
+    EXPECT_EQ(s.addBinary("0", "0"), "0");
+    EXPECT_EQ(s.addBinary("1010", "1011"), "10101");
+    EXPECT_EQ(s.addBinary("1111", "1111"), "11110");
+}
+
+TEST(Problem68Test, Basic) {
+    Solution68 s;
+
+    vector<string> v1 = {"This", "is", "an", "example", "of", "text", "justification."};
+    vector<string> r1 = {"This    is    an", "example  of text", "justification.  "};
+    EXPECT_EQ(s.fullJustify(v1, 16), r1);
+
+    vector<string> v2 = {"What","must","be","acknowledgment","shall","be"};
+    vector<string> r2 = {"What   must   be", "acknowledgment  ", "shall be        "};
+    EXPECT_EQ(s.fullJustify(v2, 16), r2);
+
+    vector<string> v3 = {"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain",
+                         "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"};
+    vector<string> r3 = {"Science  is  what we", "understand      well", "enough to explain to",
+                         "a  computer.  Art is", "everything  else  we", "do                  "};
+    EXPECT_EQ(s.fullJustify(v3, 20), r3);
 }
